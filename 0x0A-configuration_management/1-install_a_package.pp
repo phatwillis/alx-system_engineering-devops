32507184fd68899d07402723660c2feb2621c4d4
Williams Michael <williamsmicheal237@gmail.com>
2025-01-13 13:44:45 -0800

    1-install_a_package.p

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
 # Am also specifying the version of flask to install using the ensure attribute
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+package { ['Flask', 'Werkzeug']:
+  ensure   => 'installed',
+  provider => 'pip3',
+}
+
+exec { 'install flask 2.1.0':
+  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.2',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  require => Package['python3-pip'],
 }
 
